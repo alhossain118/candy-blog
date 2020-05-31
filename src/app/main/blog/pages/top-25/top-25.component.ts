98c06b337ea0b66c7539eefd6024bf2ca8552941
Alhossain118 <alhossain118@gmail.com>
2020-05-31 18:35:15 -0500

    added images of companies

--- src/app/main/blog/pages/top-25/top-25.component.ts
+++ src/app/main/blog/pages/top-25/top-25.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ArticleOverview, Articles } from '../../article-data/article-data';
+import { Companies, Company } from './top-25-data';
 @Component({
   selector: 'candy-top-25',
   templateUrl: './top-25.component.html',
@@ -10,4 +11,5 @@ export class Top25Component {
     window.scrollTo(0, 0);
   }
   public article: ArticleOverview = Articles[0];
+  public companies: Company[] = Companies;
 }
