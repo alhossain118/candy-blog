c9b6a4d3ce1aaa884179e09e4904c1b7445e659c
Alhossain118 <alhossain118@gmail.com>
2020-06-07 14:21:30 -0500

    successfully added swap btn

--- src/app/main/blog/pages/top-25/top-25.component.ts
+++ src/app/main/blog/pages/top-25/top-25.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
 import { Router } from '@angular/router';
 import { ArticleOverview, Articles } from '../../article-data/article-data';
 import { Companies, Company } from './top-25-data';
@@ -9,10 +9,15 @@ import { Companies, Company } from './top-25-data';
   styleUrls: ['./top-25.component.scss'],
 })
 export class Top25Component {
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private renderer: Renderer2,
+  ) {}
 
   public companies: Company[] = Companies;
   public p = 1;
+  public gridViewSelected = true;
+  public tableViewSelected = false;
 
   public get article(): ArticleOverview {
     return Articles.find((article: ArticleOverview) => {
@@ -24,6 +29,35 @@ export class Top25Component {
     window.scrollTo(0, 0);
     this.p = e;
   }
+
+  public changeLayoutView(view: string) {
+    if (view === 'gridViewSelected') {
+      this.gridViewSelected = true;
+      this.tableViewSelected = false;
+
+      this.renderer.addClass(
+        document.querySelector('#gridBtn'),
+        'selectedView'
+      );
+      this.renderer.removeClass(
+        document.querySelector('#tableBtn'),
+        'selectedView'
+      );
+    } else {
+      this.gridViewSelected = false;
+      this.tableViewSelected = true;
+
+      this.renderer.addClass(
+        document.querySelector('#tableBtn'),
+        'selectedView'
+      );
+      this.renderer.removeClass(
+        document.querySelector('#gridBtn'),
+        'selectedView'
+      );
+    }
+  }
+
   public routeToCompany(name: string): void {
     const link = name.split(' ').join('-');
     this.router.navigate(['../company', link]);
--- src/app/main/company/company.component.ts
+++ src/app/main/company/company.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Companies, Company } from '../blog/pages/top-25/top-25-data';
 @Component({
@@ -7,10 +8,18 @@ import { Companies, Company } from '../blog/pages/top-25/top-25-data';
   styleUrls: ['./company.component.scss'],
 })
 export class CompanyComponent implements OnInit {
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder
+  ) {}
 
   public url: string;
   public company: Company;
+  model = {
+    left: true,
+    middle: false,
+    right: false
+  };
 
   public ngOnInit(): void {
     this.route.params.subscribe((res: any) => {
